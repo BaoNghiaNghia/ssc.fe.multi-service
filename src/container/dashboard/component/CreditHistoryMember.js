08723821a80fcbb43acaf5f4123a2a743d2d8824
BaoNghiaNghia <baonghia.kht@gmail.com>
2024-06-15 15:51:04 +0700

    update filter order transaction

--- src/container/dashboard/component/CreditHistoryMember.js
+++ src/container/dashboard/component/CreditHistoryMember.js
@@ -2,18 +2,21 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch , useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import { debounce } from 'lodash';
 import { FaYoutube } from "react-icons/fa";
 import { GrTransaction } from "react-icons/gr";
-import { Row, Col, Form, Modal, Table, Tooltip, Popover } from 'antd';
+import { Row, Col, Form, Modal, Table, Tooltip, Popover, Input } from 'antd';
 import moment from 'moment';
 import memberActions from '../../../redux/member/actions';
 import serviceActions from '../../../redux/serviceSettings/actions';
 import { DEFAULT_PAGESIZE, DEFAULT_PERPAGE, VIETNAMES_CURRENCY } from '../../../variables';
 import { numberWithCommas } from '../../../utility/utility';
 
+const { Search } = Input;
+
 function CreditHistoryMember({ historyState, setState }) {
   const dispatch = useDispatch();
-  const [formDelService] = Form.useForm();
+  const [formFilterTransaction] = Form.useForm();
   
   const { isModalCreditHistory, selectedRowID } = historyState;
 
@@ -186,6 +189,36 @@ function CreditHistoryMember({ historyState, setState }) {
 
   const findUser = creditHistory?.items?.length > 0 ? userList?.filter((item) => item.id === creditHistory?.items[0]?.user_id) : [];
 
+  const handleSearchOrder = (searchText) => {
+    const { value } = searchText.target;
+
+    if (!value) {
+      dispatch(memberActions.getCreditHistoryMemberBegin({
+        page: 1,
+        limit: limitPage,
+        user_id: selectedRowID
+      }));
+    } else {
+      dispatch(memberActions.getCreditHistoryMemberBegin({
+        page: 1,
+        limit: limitPage,
+        user_id: selectedRowID,
+        order_id: value
+      }));
+    }
+
+    // const arraySearchValidate = searchText.split(',').map(s => s.trim()).filter(elm => elm != null && elm !== false && elm !== "" && elm !== '');
+
+    // if (arraySearchValidate && arraySearchValidate.length > 0) {
+    //   dispatch(memberActions.getCreditHistoryMemberBegin({
+    //     page: currentPage,
+    //     limit: limitPage,
+    //     user_id: selectedRowID,
+    //     order_id: arraySearchValidate.join(",")
+    //   }));
+    // }
+  };
+
   return (
     <>
       <Modal
@@ -193,60 +226,73 @@ function CreditHistoryMember({ historyState, setState }) {
         open={isModalCreditHistory}
         centered
         title={
-          <>
-            <div style={{ display: 'inline-flex', alignItems: 'center', alignContent: 'center' }}>
-              <GrTransaction fontSize={40} color='#a1a1a1' style={{ margin: '0 20px 0 0', padding: '5px', border: '1px solid #c5c5c5', borderRadius: '10px' }} />
-              <div >
-                <p style={{ fontSize: '1.1em', marginBottom: '2px', fontWeight: '700' }}>Danh sách giao dịch</p>
-                <p style={{ fontSize: '0.7em', marginBottom: '2px', fontWeight: '700' }}>Thống kê toàn bộ giao dịch của khách hàng</p>
-                {
-                  findUser?.length > 0 ? (
-                    <div style={{ display: 'inline-flex', alignItems: 'center' }}>
-                      <span style={{ margin: '0px', fontWeight: '700', fontSize: '0.7em', marginRight: '25px' }}>Họ tên: { findUser[0]?.fullname }</span>
-                      <span style={{ margin: '0px', fontSize: '0.7em' }}>Email: { findUser[0]?.email }</span>
-                    </div>
-                  ) : null
-                }
+          <Row gutter="10" style={{ display: 'flex', alignItems: 'end' }}>
+            <Col sm={15}>
+              <div style={{ display: 'inline-flex', alignItems: 'center', alignContent: 'center' }}>
+                <GrTransaction fontSize={40} color='#a1a1a1' style={{ margin: '0 20px 0 0', padding: '5px', border: '1px solid #c5c5c5', borderRadius: '10px' }} />
+                <div >
+                  <p style={{ fontSize: '1.1em', marginBottom: '2px', fontWeight: '700' }}>Danh sách giao dịch</p>
+                  {/* <p style={{ fontSize: '0.7em', marginBottom: '2px', fontWeight: '700' }}>Thống kê toàn bộ giao dịch của khách hàng</p> */}
+                  {
+                    findUser?.length > 0 ? (
+                      <div style={{ display: 'inline-flex', alignItems: 'center' }}>
+                        <span style={{ margin: '0px', fontWeight: '700', fontSize: '0.7em', marginRight: '25px' }}>Họ tên: { findUser[0]?.fullname }</span>
+                        <span style={{ margin: '0px', fontSize: '0.7em' }}>Email: { findUser[0]?.email }</span>
+                      </div>
+                    ) : null
+                  }
+                </div>
               </div>
-            </div>
-          </>
+            </Col>
+            <Col sm={8}>
+              <Form layout="horizontal" form={formFilterTransaction}>
+                <Form.Item
+                  name="order_id"
+                  label="Tìm kiếm"
+                  style={{ margin: 0, padding: 0 }}
+                >
+                  <Search 
+                    size='small'
+                    style={{ width: '90%', margin: 0 }}
+                    placeholder='Tìm theo Order ID'
+                    onChange={debounce(handleSearchOrder, 500)}
+                    allowClear
+                  />
+                </Form.Item>
+              </Form>
+            </Col>
+          </Row>
         }
         onCancel={handleCancel}
         footer={null}
       >
-        <Form name="add_service" layout="vertical" form={formDelService}>
-          <Row gutter="10">
-            <Col sm={24}>
-              <Table
-                size='small'
-                loading={isLoading}
-                dataSource={dataSource}
-                columns={columns}
-                pagination={{
-                  current: creditHistory?.meta?.current_page,
-                  defaultPageSize: creditHistory?.meta?.count,
-                  pageSize: creditHistory?.meta?.per_page,
-                  total: creditHistory?.meta?.total,
-                  showSizeChanger: true,
-                  pageSizeOptions: DEFAULT_PAGESIZE,
-                  onChange(page, pageSize) {
-                      setCurrentPage(page);
-                      setLimitPage(pageSize)
-                  },
-                  position: ['bottomCenter'],
-                  responsive: true,
-                  showTotal(total, range) {
-                      return <>
-                          <p className='mx-4'>Tổng cộng <span style={{ fontWeight: 'bold' }}>{numberWithCommas(total || 0)}</span> giao dịch</p>
-                      </>
-                  },
-                  totalBoundaryShowSizeChanger: 100,
-                  size: "small"
-                }}
-              />
-            </Col>
-          </Row>
-        </Form>
+        <Table
+          size='small'
+          loading={isLoading}
+          dataSource={dataSource}
+          columns={columns}
+          pagination={{
+            current: creditHistory?.meta?.current_page,
+            defaultPageSize: creditHistory?.meta?.count,
+            pageSize: creditHistory?.meta?.per_page,
+            total: creditHistory?.meta?.total,
+            showSizeChanger: true,
+            pageSizeOptions: DEFAULT_PAGESIZE,
+            onChange(page, pageSize) {
+                setCurrentPage(page);
+                setLimitPage(pageSize)
+            },
+            position: ['bottomCenter'],
+            responsive: true,
+            showTotal(total, range) {
+                return <>
+                    <p className='mx-4'>Tổng cộng <span style={{ fontWeight: 'bold' }}>{numberWithCommas(total || 0)}</span> giao dịch</p>
+                </>
+            },
+            totalBoundaryShowSizeChanger: 100,
+            size: "small"
+          }}
+        />
       </Modal>
     </>
   );
